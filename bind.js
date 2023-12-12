40d941bf94fa78840fbbadf2bcdf02ea874c7428
prathmesh <prathmeshpol61@gmail.com>
2023-12-12 15:30:00 +0530

    change

--- bind.js
+++ bind.js
@@ -8,29 +8,10 @@ function myFun(id) {
 }
 
 Function.prototype.myBind = function (obj, ...arg) {
-  let func = this;
-  return function () {
-    func.apply(obj, [...arg]);
+  obj.fn = this;
+  return function (...newArg) {
+    return obj.fn(...arg, ...newArg);
   };
 };
-let neBind = myFun.myBind(obj, "myID@@");
+let neBind = myFun.myBind(obj, "870@@");
 neBind();
-
-// try 2
-let object = {
-  name: "Prathmesh",
-};
-
-function regular(myID) {
-  return console.log(`${this.name} and ${myID}`);
-}
-
-Function.prototype.newMyBind = function (object, ...arg) {
-  let thisVal = this;
-  return function () {
-    thisVal.apply(object, [...arg]);
-  };
-};
-
-let bjj = regular.newMyBind(object, "987666");
-bjj();
